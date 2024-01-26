c31107c33c502cee644935194a7c0db7564e8946
M. KALEEMULLAH KHAN QURESHI <kaleemullahqurashi29@gmail.com>
2024-01-26 07:02:41 +0500

    stopwatch by using javascript

---
+++ app.js
@@ -0,0 +1,32 @@
+var timer = 0;
+var timerInterval;
+var ms = document.getElementById('ms');
+var second = document.getElementById('second');
+var minute = document.getElementById('minute');
+
+function start() {
+  stop();
+  timerInterval = setInterval(function() {
+    timer += 1/60;
+    msVal = Math.floor((timer - Math.floor(timer))*100);
+    secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
+    minuteVal = Math.floor(timer/60);
+    ms.innerHTML = msVal < 10 ? "0" + msVal.toString() : msVal;
+    second.innerHTML = secondVal < 10 ? "0" + secondVal.toString() : secondVal;
+    minute.innerHTML = minuteVal < 10 ? "0" + minuteVal.toString() : minuteVal;
+  }, 1000/60);
+}
+
+function stop() {
+  clearInterval(timerInterval);
+}
+function resetStopwatch() {
+    stop(); // Stop the stopwatch
+  
+    // Reset the timer variables
+    timer = 0;
+    ms.innerHTML = "00";
+    second.innerHTML = "00";
+    minute.innerHTML = "00";
+  }
+  
